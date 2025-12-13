0ceb5ec14cfc4654dafabcf0294b0669a7019c4f
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2025-12-13 12:36:30 +0530

    Do initial setup for frontend and backend

---
+++ server/src/connection/index.js
@@ -0,0 +1,3 @@
+import connectToDatabase from './mongodb.connect.js';
+
+export {connectToDatabase};
---
+++ server/src/controllers/checkHealth.controller.js
@@ -0,0 +1,12 @@
+import {asyncHandler, ApiResponse} from '../utility/index.js';
+import statusCode from '../constants/statusCode.js';
+
+const checkHealth = asyncHandler(async (req, res) => {
+    return res
+        .status(statusCode.OK)
+        .json(new ApiResponse(statusCode.OK, 'Server is running!', {
+            test: process.env.TEST
+        }));
+});
+
+export default checkHealth;
