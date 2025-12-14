417a715e1d5957038df261dade08fafcfb191949
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2025-12-14 11:14:44 +0530

    Add relationship establishment page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,5 +2,6 @@ import Home from './Home.jsx';
 import About from './About';
 import NotFound from './NotFound';
 import Authentication from './Authentication.jsx';
+import Relations from './Relations.jsx';
 
-export {Home, About, NotFound, Authentication};
+export {Home, About, NotFound, Authentication, Relations};
