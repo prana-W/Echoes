5197584df32086400d51b5963bd576a714d1774e
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2025-12-14 13:21:50 +0530

    Increase limit for video handling

--- server/src/middlewares/timeCapsule/uploadVideo.middleware.js
+++ server/src/middlewares/timeCapsule/uploadVideo.middleware.js
@@ -3,7 +3,7 @@ import createUploader from '../../utility/uploadMediaTimeCapsule.util.js';
 const uploadVideos = createUploader({
     allowedMimePrefix: 'video/',
     maxFileSize: 300 * 1024 * 1024, // 300MB
-    maxFiles: 1,
+    maxFiles: 5,
 });
 
 export default uploadVideos;
